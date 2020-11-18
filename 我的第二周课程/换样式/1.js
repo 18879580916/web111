a1b2527d1d2ac9d9e48749aa11f46286ad16d6fb
18879580916 <1476579425@qq.com>
2020-11-18 17:58:33 +0800

    第十六次课

---
+++ 我的第二周课程/换样式/1.js
@@ -0,0 +1,14 @@
+var apple = function(){
+    console.log('error');
+
+}
+apple()
+function orange(){
+    console.log('error orange');
+
+}
+orange()
+function redorange(){
+    console.log('error redorange');
+
+
